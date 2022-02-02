c912842739367b792784b7a026457cea4e04b231
Adarsh Gaur <adarshgaur3@gmail.com>
2022-02-02 18:02:05 +0530

    added admin customizations

--- Backend/blog/admin.py
+++ Backend/blog/admin.py
@@ -1,5 +1,5 @@
-import imp
 from django.contrib import admin
+from django.contrib import messages
 
 from .models import(
 	MyUser,
@@ -8,7 +8,27 @@ from .models import(
 	OtpModel,
 )
 
-admin.site.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+	list_display = ('username', 'email', 'id', 'pk', 'active', 'date_joined')
+	
+	def active(self, user):
+		return user.is_active
+	
+	active.boolean = True
+	
+	def make_active(modeladmin, request, queryset):
+		queryset.update(is_active = 1)
+		messages.success(request, "Selected Record(s) Marked as Active Successfully !!")
+	
+	def make_inactive(modeladmin, request, queryset):
+		queryset.update(is_active = 0)
+		messages.success(request, "Selected Record(s) Marked as Inactive Successfully !!")
+	
+	admin.site.add_action(make_active, "Make Active")
+	admin.site.add_action(make_inactive, "Make Inactive")
+
+
+admin.site.register(MyUser, MyUserAdmin)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(OtpModel)
