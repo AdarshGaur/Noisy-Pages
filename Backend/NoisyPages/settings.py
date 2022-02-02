cc2d1c0358e09a009b318a0fdcf89e1c9c85f43a
Adarsh Gaur <adarshgaur3@gmail.com>
2022-02-02 18:03:36 +0530

    added jwt confs

--- Backend/NoisyPages/settings.py
+++ Backend/NoisyPages/settings.py
@@ -147,3 +147,31 @@ EMAIL_HOST_USER = "noisypages.sc@gmail.com"
 EMAIL_HOST_PASSWORD = "JJh6rHkTSfiURRF"
 EMAIL_PORT = 587
 
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+	'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+	'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+	'ROTATE_REFRESH_TOKENS': False,
+	'BLACKLIST_AFTER_ROTATION': True,
+
+	'ALGORITHM': 'HS256',
+	'SIGNING_KEY': SECRET_KEY,
+	'VERIFYING_KEY': None,
+	'AUDIENCE': None,
+	'ISSUER': None,
+
+	'AUTH_HEADER_TYPES': ('Bearer',),
+	'USER_ID_FIELD': 'id',
+	'USER_ID_CLAIM': 'user_id',
+
+	'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+	'TOKEN_TYPE_CLAIM': 'token_type',
+
+	'JTI_CLAIM': 'jti',
+
+	'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+	'SLIDING_TOKEN_LIFETIME': timedelta(minutes=180),
+	'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
+}
