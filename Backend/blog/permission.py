603898af8f05763cb866adf6254b29faec34e4bf
Adarsh Gaur <adarshgaur3@gmail.com>
2022-02-04 23:13:19 +0530

    added custom permission class

---
+++ Backend/blog/permission.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+	def has_object_permission(self, request, view, obj):
+		if obj.author == request.user or request.method in permissions.SAFE_METHODS:
+			return True
+		return False
+
