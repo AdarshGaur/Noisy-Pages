3b1e871e43a1657a9922cf053a4c35203681e13d
Adarsh Gaur <adarshgaur3@gmail.com>
2022-08-26 22:43:41 +0530

    added Post Cards

--- frontend/src/posts/Posts.jsx
+++ frontend/src/posts/Posts.jsx
@@ -1,9 +1,17 @@
+import Post from "../post/Post"
 import "./posts.css"
 
 export default function Posts(){
 	return (
 		<div className="posts">
-			Posts
+			<Post/>
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
 		</div>
 	)
 }
