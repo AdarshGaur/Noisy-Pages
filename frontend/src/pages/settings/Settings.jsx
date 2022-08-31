ab1f5dc1682e3386b5e9cd6c308e8890d0dade04
Adarsh Gaur <adarshgaur3@gmail.com>
2022-08-31 20:28:14 +0530

    added more page components

---
+++ frontend/src/pages/settings/Settings.jsx
@@ -0,0 +1,44 @@
+import "./settings.css"
+import Sidebar from "../../sidebar/Sidebar"
+
+
+export default function Settings() {
+	return (
+		<div className="settings">
+			<div className="settingsWrapper">
+				<div className="settingsTitle">
+					<span className="settingsTitleUpdate">Update Your Account</span>
+					<span className="settingsTitleDelete">Delete Account</span>
+				</div>
+				<form className="settingsForm">
+					<label>Profile Picture</label>
+					<div className="settingsPP">
+						<img
+							src=""
+							alt="profile-pp"
+						/>
+						<label htmlFor="fileInput">
+						<i className="settingsPPIcon far fa-user-circle"></i>{" "}
+						</label>
+						<input
+						id="fileInput"
+						type="file"
+						style={{ display: "none" }}
+						className="settingsPPInput"
+						/>
+					</div>
+					<label>Username</label>
+					<input type="text" placeholder="Your Name" name="name" />
+					<label>Email</label>
+					<input type="email" placeholder="author@yahoo.com" name="email" />
+					<label>Password</label>
+					<input type="password" placeholder="Password" name="password" />
+					<button className="settingsSubmitButton" type="submit">
+						Update
+					</button>
+				</form>
+			</div>
+			<Sidebar />
+		</div>
+	);
+}
