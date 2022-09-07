983e6c910936f8db097780372bdf57d53f44ad9b
Adarsh Gaur <adarshgaur3@gmail.com>
2022-09-07 23:55:17 +0530

    added login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
-import Home from "./pages/home/Home";
 import Topbar from "./topbar/topbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Register from "./pages/registser/Register";
+import Login from "./pages/login/Login";
+import Settings from "./pages/settings/Settings";
+import Write from "./pages/write/Write";
+import Single from "./pages/single/Single";
 
 
 function App() {
-  return (
-    <>
-    <Topbar />
-    <Home />
-    </>
-  );
+	const currentUser = false
+	return (
+		<Router>
+			<Topbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path="/posts" element={<Home />} />
+				<Route path="/register" element={currentUser ? <Home /> : <Register/>} />
+				<Route path="/login" element={currentUser ? <Home /> : <Login/>} />
+				<Route path="/post/:id" element={<Single />} />
+				<Route path="/write" element={currentUser ? <Write /> : <Login />} />
+				<Route path="/settings" element={currentUser ? <Settings /> : <Login />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- frontend/src/topbar/topbar.jsx
+++ frontend/src/topbar/topbar.jsx
@@ -1,32 +1,62 @@
 import React from "react"
 import "./topbar.css"
 import logo from "../assets/logo.png"
+import { Link } from "react-router-dom"
 
 export default function Topbar(){
+	const currentUser = false
 	return(
 		<div className="top">
-			<div className="topRight">
-				<img
-					className="topImage"
-					src = {logo}
-					alt="logo" 
-				/>
-				<i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
-			</div>
-			<div className="topCenter">
-				<ul className="topList">
-					<li className="topListItem">HOME</li>
-					<li className="topListItem">ABOUT</li>
-					<li className="topListItem">CONTACT</li>
-					<li className="topListItem">WRITE</li>
-					<li className="topListItem">LOGOUT</li>
-				</ul>
-			</div>
 			<div className="topLeft">
 				<i className="topIcon fa-brands fa-instagram"></i>
 				<i className="topIcon fa-brands fa-facebook-f"></i>
 				<i className="topIcon fa-brands fa-twitter"></i>
 				<i className="topIcon fa-brands fa-pinterest-p"></i>
+			</div>
+			<div className="topCenter">
+				<ul className="topList">
+					<li className="topListItem">
+						<Link className="link" to="/">HOME</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/">ABOUT</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/">CONTACT</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/write">WRITE</Link>
+					</li>
+					<li className="topListItem">
+						{currentUser && "LOGOUT"}
+					</li>
+				</ul>
+			</div>
+			<div className="topRight">
+				{currentUser ? (
+					<Link className="link" to="/settings">
+					<img
+						className="topImage"
+						src = {logo}
+						alt="logo" 
+					/>
+					</Link>
+					) : (
+						<ul className="topList">
+							<li className="topListItem">
+								<Link className="link" to="/login">
+									SIGN IN
+								</Link>
+							</li>
+							<li className="topListItem">
+								<Link className="link" to="/register">
+									SIGN UP
+								</Link>
+							</li>
+						</ul>
+					)
+				}
+				<i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
 			</div>
 		</div>
 	)
